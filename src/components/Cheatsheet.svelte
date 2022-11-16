0f3967c37f542b56f9cdbca8e84ea950771b3263
thereal-atom <oscarfal2006@gmail.com>
2022-11-16 23:05:04 +0000

    more cheats to neovim

--- src/components/Cheatsheet.svelte
+++ src/components/Cheatsheet.svelte
@@ -8,21 +8,23 @@
     class="sm:px-16 px-8"
     id={data.title}
 >
-    <p class="font-black sm:text-5xl text-4xl sm:my-20 my-12">{data.title}</p>
-    <p class="font-bold text-2xl my-4 underline">Key</p>
-    {#if data.key}
-        {#each data.key as key}
-            <p>{key.key} - {key.meaning}</p>
-        {/each}
-    {/if}
+    <div class="mb-8">
+        <p class="font-black sm:text-5xl text-4xl sm:my-20 my-12">{data.title}</p>
+        <p class="font-bold text-2xl my-4 underline">Key</p>
+        {#if data.key}
+            {#each data.key as key}
+                <p>{key.key} - {key.meaning}</p>
+            {/each}
+        {/if}
+    </div>
     {#each data.sections as section}
         <p class="font-bold text-2xl my-4 underline">{section.title}</p>
-        <div class="flex flex-col">
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
             {#each section.cheats as cheat}
-                <div class="flex flex-col">
-                    <p class="font-black">{cheat.title}</p>
+                <div class="flex flex-col md:mb-16 mb-8">
+                    <code class="font-black text-xs bg-gray-800 w-fit px-2 py-1 rounded-md mb-2">{cheat.title}</code>
                     {#each cheat.methods as method}
-                        <p>{method}</p>
+                        <p class="font-bold my-1">{method}</p>
                     {/each}
                 </div>
             {/each}
