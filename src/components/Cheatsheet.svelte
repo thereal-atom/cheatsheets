93ab487f2be0425b84d4cc6ae019b6a1e8bd27a2
thereal-atom <oscarfal2006@gmail.com>
2022-11-19 17:28:17 +0000

    more cheats

--- src/components/Cheatsheet.svelte
+++ src/components/Cheatsheet.svelte
@@ -2,14 +2,26 @@
     import type Data from "../types/data";
         
     export let data: Data;
-</script>
 
+    const doesStartWithPrefix = (text: string, prefixes: {
+        key: string;
+        meaning: string;
+    }[]) => {
+        let startsWithPrefix = false;
+
+        prefixes.forEach(prefix => {
+            if (text.startsWith(prefix.key)) startsWithPrefix = true;
+        });
+
+        return startsWithPrefix;
+    };
+</script>
 <div
     class="sm:px-16 px-8"
     id={data.title}
 >
     <div class="mb-8">
-        <p class="font-black sm:text-5xl text-4xl sm:my-20 my-12">{data.title}</p>
+        <p class="font-black sm:text-5xl text-4xl sm:my-20 my-12 text-indigo-500">{data.title}</p>
         <p class="font-bold text-2xl my-4 underline">Key</p>
         {#if data.key}
             {#each data.key as key}
@@ -24,7 +36,11 @@
                 <div class="flex flex-col md:mb-16 mb-8">
                     <code class="font-black text-xs bg-gray-800 w-fit px-2 py-1 rounded-md mb-2">{cheat.title}</code>
                     {#each cheat.methods as method}
-                        <p class="font-bold my-1">{method}</p>
+                        {#if method === "break"}
+                            <p class="text-transparent">x</p>
+                        {:else}
+                            <p class="font-bold my-1 {doesStartWithPrefix(method, data.key ?? []) || method.startsWith(" ") || !data.key ? "" : "text-gray-600 font-black"}">{method}</p>
+                        {/if}
                     {/each}
                 </div>
             {/each}
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    $: isNavbarActive = false;
+</script>
+
+<div class="w-full fixed bg-black left-0 p-3 border-b border-solid flex flex-col">
+	<p
+        class="text-white font-black hover:cursor-pointer transition-all"
+        on:click={() => isNavbarActive = !isNavbarActive}
+    >
+        #
+    </p>
+    {#if isNavbarActive}
+        <div class="mt-4">
+            <a class="text-red-50">nav</a>
+        </div>
+    {/if}
+</div>
